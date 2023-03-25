ff99b82b4de3af880325e3b280ed0946cde3a24e
YALIM GÜRBÜZ <yalim1999@hotmail.com>
2023-03-25 09:29:41 +0300

    INSTAGRAM APP

--- src/firebase.js
+++ src/firebase.js
@@ -9,7 +9,7 @@ import {
 } from "firebase/auth";
 import toast from "react-hot-toast";
 import { userHandle } from "./utils";
-import { doc, getDoc, getFirestore, setDoc } from "firebase/firestore"
+import { doc, getDoc, getFirestore, setDoc } from "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
@@ -17,76 +17,78 @@ const firebaseConfig = {
     projectId: process.env.REACT_APP_PROJECT_ID,
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID
+    appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth()
-const db = getFirestore(app)
+const auth = getAuth();
+const db = getFirestore(app);
 
-onAuthStateChanged(auth, async user => {
+onAuthStateChanged(auth, async (user) => {
     if (user) {
-        const dbUser = await getDoc(doc(db, "users", user.uid))
+        const dbUser = await getDoc(doc(db, "users", user.uid));
         let data = {
             uid: user.uid,
             fullName: user.displayName,
             email: user.email,
             emailVerified: user.emailVerified,
-            ...dbUser.data()
-        }
-        userHandle(data)
+            ...dbUser.data(),
+        };
+        userHandle(data);
     } else {
-        userHandle(false)
+        userHandle(false);
     }
-})
+});
 
 export const login = async (email, password) => {
     try {
-        return await signInWithEmailAndPassword(auth, email, password)
+        return await signInWithEmailAndPassword(auth, email, password);
     } catch (err) {
-        toast.error(err.code)
+        toast.error(err.code);
     }
-}
+};
 
 export const register = async ({ email, password, full_name, username }) => {
     try {
-        const user = await getDoc(doc(db, "usernames", username))
+        const user = await getDoc(doc(db, "usernames", username));
         if (user.exists()) {
-            toast.error(`${username} kullanıcı adı başkası tarafından kullanılıyor.`)
+            toast.error(`${username} kullanıcı adı başkası tarafından kullanılıyor.`);
         } else {
-            const response = await createUserWithEmailAndPassword(auth, email, password)
+            const response = await createUserWithEmailAndPassword(
+                auth,
+                email,
+                password
+            );
             if (response.user) {
-
                 await setDoc(doc(db, "usernames", username), {
-                    user_id: response.user.uid
-                })
+                    user_id: response.user.uid,
+                });
 
                 await setDoc(doc(db, "users", response.user.uid), {
                     fullName: full_name,
                     username: username,
                     followers: [],
                     following: [],
-                    notifications: []
-                })
+                    notifications: [],
+                });
 
                 await updateProfile(auth.currentUser, {
-                    displayName: full_name
-                })
+                    displayName: full_name,
+                });
 
-                return response.user
-
+                return response.user;
             }
         }
     } catch (err) {
-        toast.error(err.code)
+        toast.error(err.code);
     }
-}
+};
 
 export const logout = async () => {
     try {
-        await signOut(auth)
+        await signOut(auth);
     } catch (err) {
-        toast.error(err.code)
+        toast.error(err.code);
     }
-}
+};
